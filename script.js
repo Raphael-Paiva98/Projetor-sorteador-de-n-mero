845be8c74f4292d71f06ebb4c8cf38d3f4d2993a
Raphael-Paiva98 <raphaelpaiva748@gmail.com>
2024-02-16 09:26:11 -0300

    corrigido erro do css

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-function generateNumber(){
-const min = Math.ceil(document.querySelector(".input-min").value)
-const max = Math.floor(document.querySelector(".input-max").value)
+function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
 
-const result = Math.floor(Math.random() * (max - min) + min);
+    if (min >= max) {
+        alert("O valor mínimo deve ser MENOR que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min) + min);
 
-const resultP = document.querySelector (".result-text")
-resultP.textContent = `O número sorteado é: ${result}`;
- 
+        const resultP = document.querySelector(".result-text")
+        resultP.textContent = `O número sorteado é: ${result}`;
+
+
+    }
 
 }
-
-
 
